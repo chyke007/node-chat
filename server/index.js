9f57259de3b7d1d2ee78a20afb106bcd5325ba91
chyke007 <chibuike@zercomsystems.com>
2020-03-28 15:16:08 +0100

    Fixes port issue

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const path = require("path")
 const express = require("express")
 const publicPath = path.join(__dirname,'../public')
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 let app = express();
 
 app.use(express.static(publicPath))
