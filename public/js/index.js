550625299f74f01e7c8fb1226c63340c36c2665d
chyke007 <chibuike@zercomsystems.com>
2020-03-29 20:56:05 +0100

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -30,11 +30,12 @@ socket.on('newLocationMessage', function(message){
 jQuery('#message-form').on('submit', function(e){
 	e.preventDefault()
 
+	var messageTextbox = jQuery('[name=message]')
 	socket.emit('createMessage', {
 		from: 'User',
-		text: jQuery('[name=message]').val()
+		text: messageTextbox.val()
 	},function(e){
-		// console.log(e)
+		messageTextbox.val('')
 	})
 })
 
@@ -44,12 +45,16 @@ locationButton.on('click', function(){
 		return alert("Geoloction not supported by your browser")
 	}
 
+
+	locationButton.attr('disabled','disabled').text('Sending location...')
 	navigator.geolocation.getCurrentPosition(function(position){
+		locationButton.removeAttr('disabled').text('Send location')
 		socket.emit('createLocationMessage',{
 			latitude: position.coords.latitude,
 			longitude: position.coords.longitude
 		});
 	},function(err){
-			alert('Unable to fetch location')
+		locationButton.removeAttr('disabled').text('Send location')
+		alert('Unable to fetch location')
 	})
 })
