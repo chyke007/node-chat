74267ff727b6126088cf2f2367487a647d2d6705
chyke007 <chibuike@zercomsystems.com>
2020-03-30 13:04:55 +0100

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,20 @@
 var socket = io();
+
+function scrollToBottom(){
+	//Selectors
+	var messages = jQuery('#messages');
+	var newMessage = messages.children('li:last-child')
+	//Heights
+	var clientHeight = messages.prop('clientHeight');
+	var scrollTop = messages.prop('scrollTop');
+	var scrollHeight = messages.prop('scrollHeight');
+	var newMessageHeight = newMessage.innerHeight();
+	var lastMessageHeight = newMessage.innerHeight();
+	if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight  >= scrollHeight){
+		messages.scrollTop(scrollHeight)
+	}
+}
+
 socket.on('connect', function(){
 	console.log('Connected to server')
 
@@ -18,6 +34,7 @@ socket.on('newMessage', function(message){
 	});
 
 	jQuery('#messages').append(html)
+	scrollToBottom()
 	// var formattedTime = moment(message.createdAt).format('h:mm a')
 	// var li = jQuery('<li></li>')
 	// li.text(`${message.from} ${formattedTime}: ${message.text}`)
@@ -35,6 +52,7 @@ socket.on('newLocationMessage', function(message){
 	});
 
 	jQuery('#messages').append(html)
+	scrollToBottom()
 	// var li = jQuery('<li></li>')
 	// var a = jQuery('<a target="_blank">My current location </a>')
 
